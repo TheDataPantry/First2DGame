179380779205996cc6a49db35f5973b24dd6ebfb
Akshay Sharma <akshaysharmastats@gmail.com>
2025-07-18 00:13:27 -0400

    Added player animations

--- Platformer Tutorial/new_game_project/Scene/lobby_level.gd
+++ Platformer Tutorial/new_game_project/Scene/lobby_level.gd
@@ -1,10 +1,9 @@
 extends Node2D
 
-@onready var player = $Player/Camera2D
+@onready var playe_camera = $Player/Camera2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player_camera.enabled = true
-
+	player_camera.enabled = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
